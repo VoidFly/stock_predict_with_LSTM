6afce40eaa06d5ee1c565348a63cca3ad233d675
VoidFly <48577190+VoidFly@users.noreply.github.com>
2020-08-25 14:25:31 +0800

    -

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 # -*- coding: UTF-8 -*-
-"""
-@author: hichenway
-@contact: lyshello123@163.com
-@time: 2020/5/9 17:00
-@license: Apache
-主程序：包括配置，数据读取，日志记录，绘图，模型训练和预测
-"""
-
+# @ lyh
 import pandas as pd
 import numpy as np
 import os
@@ -16,54 +9,50 @@ import logging
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-frame = "pytorch"  # 可选： "keras", "pytorch", "tensorflow"
-if frame == "pytorch":
-    from model.model_pytorch import train, predict
-elif frame == "keras":
-    from model.model_keras import train, predict
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'
-elif frame == "tensorflow":
-    from model.model_tensorflow import train, predict
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'    # tf和keras下会有很多tf的warning，但不影响训练
-else:
-    raise Exception("Wrong frame seletion")
+frame = "pytorch"
+from model.model_pytorch import train, predict
+
 
 class Config:
     # 数据参数
-    feature_columns = list(range(2, 9))     # 要作为feature的列，按原数据从0开始计算，也可以用list 如 [2,4,6,8] 设置
-    label_columns = [4, 5]                  # 要预测的列，按原数据从0开始计算, 如同时预测第四，五列 最低价和最高价
-    # label_in_feature_index = [feature_columns.index(i) for i in label_columns]  # 这样写不行
-    label_in_feature_index = (lambda x,y: [x.index(i) for i in y])(feature_columns, label_columns)  # 因为feature不一定从0开始
+    feature_columns = list(range(1, 5))     # 要作为feature的列,index列不算，第一列数据为第0列
+    label_columns = [0]                  # 要预测的列
+
+    up_threshold=0.05
+    down_threshold=-0.02
 
     predict_day = 1             # 预测未来几天
 
     # 网络参数
     input_size = len(feature_columns)
-    output_size = len(label_columns)
+    
+    output_size = 3
 
     hidden_size = 128           # LSTM的隐藏层大小，也是输出大小
     lstm_layers = 2             # LSTM的堆叠层数
     dropout_rate = 0.2          # dropout概率
-    time_step = 20              # 这个参数很重要，是设置用前多少天的数据来预测，也是LSTM的time step数
+    time_step = 20              # 用前多少天的数据来预测，也是LSTM的time step数
 
     # 训练参数
     do_train = True
     do_predict = True
+
     add_train = False           # 是否载入已有模型参数进行增量训练
     shuffle_train_data = True   # 是否对训练数据做shuffle
-    use_cuda = False            # 是否使用GPU训练
+    use_cuda = True            # 是否使用GPU训练
 
-    train_data_rate = 0.95      # 训练数据占总体数据比例，测试数据就是 1-train_data_rate
-    valid_data_rate = 0.15      # 验证数据占训练数据比例，验证集在训练过程使用，为了做模型和参数选择
+    train_data_rate = 0.99      # 训练数据占总体数据比例，测试数据就是 1-train_data_rate
+    valid_data_rate = 0.01      # 验证数据占训练数据比例，验证集在训练过程使用，为了做模型和参数选择
 
     batch_size = 64
     learning_rate = 0.001
-    epoch = 20                  # 整个训练集被训练多少遍，不考虑早停的前提下
+    epoch = 5                  # 整个训练集被训练多少遍，不考虑早停的前提下
     patience = 5                # 训练多少epoch，验证集没提升就停掉
     random_seed = 42            # 随机种子，保证可复现
 
-    do_continue_train = False    # 每次训练把上一次的final_state作为下一次的init_state，仅用于RNN类型模型，目前仅支持pytorch
+    do_continue_train = False    # 每次训练把上一次的final_state作为下一次的init_state，仅用于RNN类型模型
     continue_flag = ""           # 但实际效果不佳，可能原因：仅能以 batch_size = 1 训练
+
     if do_continue_train:
         shuffle_train_data = False
         batch_size = 1
@@ -71,21 +60,22 @@ class Config:
 
     # 训练模式
     debug_mode = False  # 调试模式下，是为了跑通代码，追求快
-    debug_num = 500  # 仅用debug_num条数据来调试
+    debug_num = 1000  # 仅用debug_num条数据来调试
 
     # 框架参数
-    used_frame = frame  # 选择的深度学习框架，不同的框架模型保存后缀不一样
-    model_postfix = {"pytorch": ".pth", "keras": ".h5", "tensorflow": ".ckpt"}
+    used_frame = frame
+    model_postfix = {"pytorch": ".pth"}
     model_name = "model_" + continue_flag + used_frame + model_postfix[used_frame]
 
     # 路径参数
-    train_data_path = "./data/stock_data.csv"
+    train_data_path = "./data/hs300_processed.csv"
     model_save_path = "./checkpoint/" + used_frame + "/"
     figure_save_path = "./figure/"
     log_save_path = "./log/"
     do_log_save = True                  # 是否将config和训练过程记录到log
     do_figure_save = False
-    do_train_visualized = False          # 训练loss可视化，pytorch用visdom，tf用tensorboardX，实际上可以通用, keras没有
+    do_train_visualized = False        # 训练loss可视化，pytorch采用visdom
+
     if not os.path.exists(model_save_path):
         os.makedirs(model_save_path)    # makedirs 递归创建目录
     if not os.path.exists(figure_save_path):
@@ -99,70 +89,111 @@ class Config:
 class Data:
     def __init__(self, config):
         self.config = config
-        self.data, self.data_column_name = self.read_data()
-
-        self.data_num = self.data.shape[0]
+        self.x_data, self.data_column_name,self.y_data = self.read_data()
+        
+        self.data_num = self.x_data.shape[0]
         self.train_num = int(self.data_num * self.config.train_data_rate)
 
-        self.mean = np.mean(self.data, axis=0)              # 数据的均值和方差
-        self.std = np.std(self.data, axis=0)
-        self.norm_data = (self.data - self.mean)/self.std   # 归一化，去量纲
+        # self.mean = np.mean(self.x_data, axis=0)
+        # self.std = np.std(self.x_data, axis=0)
+        # self.norm_data = (self.x_data - self.mean)/self.std   # 归一化，去量纲
+
+        #self.mean_y = np.mean(self.y_data, axis=0)
+        #self.std_y = np.std(self.y_data, axis=0)
+        #self.y_data = (self.y_data - self.mean_y)/self.std_y   # 归一化，去量纲
+
 
         self.start_num_in_test = 0      # 测试集中前几天的数据会被删掉，因为它不够一个time_step
+    
+    # def mean_std(self):
+    #     return self.mean,self.std
 
     def read_data(self):                # 读取初始数据
         if self.config.debug_mode:
-            init_data = pd.read_csv(self.config.train_data_path, nrows=self.config.debug_num,
-                                    usecols=self.config.feature_columns)
+            init_data = pd.read_csv(self.config.train_data_path,
+                                    nrows=self.config.debug_num,index_col=0)
         else:
-            init_data = pd.read_csv(self.config.train_data_path, usecols=self.config.feature_columns)
-        return init_data.values, init_data.columns.tolist()     # .columns.tolist() 是获取列名
+            init_data = pd.read_csv(self.config.train_data_path,index_col=0)
+        
+        return init_data.iloc[:,self.config.feature_columns].to_numpy(), init_data.columns.tolist(), init_data.iloc[:,self.config.label_columns].to_numpy()
+
+    def categorize(self, x):
+        if x>self.config.up_threshold:
+            return 2
+        elif x<self.config.down_threshold: 
+            return 0
+        else :
+            return 1
+
 
     def get_train_and_valid_data(self):
-        feature_data = self.norm_data[:self.train_num]
-        label_data = self.norm_data[self.config.predict_day : self.config.predict_day + self.train_num,
-                                    self.config.label_in_feature_index]    # 将延后几天的数据作为label
+        feature_data = self.x_data[:self.train_num]
+        label_data=self.y_data[:self.train_num+self.config.predict_day]
 
+        rolling_win=[]
+        train_x=[]
+        train_y=[]
         if not self.config.do_continue_train:
             # 在非连续训练模式下，每time_step行数据会作为一个样本，两个样本错开一行，比如：1-20行，2-21行。。。。
-            train_x = [feature_data[i:i+self.config.time_step] for i in range(self.train_num-self.config.time_step)]
-            train_y = [label_data[i:i+self.config.time_step] for i in range(self.train_num-self.config.time_step)]
+            #train_x = [feature_data[i:i+self.config.time_step] for i in range(self.train_num-self.config.time_step)]
+            #train_y = [label_data[i:i+self.config.time_step] for i in range(self.train_num-self.config.time_step)]
+            for i in range(self.train_num):
+                rolling_win.append(feature_data[i])
+                if len(rolling_win)==self.config.time_step:
+                    train_x.append(rolling_win)
+                    rolling_win=rolling_win[1:]
+                    train_y.append((label_data[i+self.config.predict_day]-label_data[i])/label_data[i])
         else:
-            # 在连续训练模式下，每time_step行数据会作为一个样本，两个样本错开time_step行，
-            # 比如：1-20行，21-40行。。。到数据末尾，然后又是 2-21行，22-41行。。。到数据末尾，……
-            # 这样才可以把上一个样本的final_state作为下一个样本的init_state，而且不能shuffle
-            # 目前本项目中仅能在pytorch的RNN系列模型中用
-            train_x = [feature_data[start_index + i*self.config.time_step : start_index + (i+1)*self.config.time_step]
-                       for start_index in range(self.config.time_step)
-                       for i in range((self.train_num - start_index) // self.config.time_step)]
-            train_y = [label_data[start_index + i*self.config.time_step : start_index + (i+1)*self.config.time_step]
-                       for start_index in range(self.config.time_step)
-                       for i in range((self.train_num - start_index) // self.config.time_step)]
+            raise('not implemented')
 
         train_x, train_y = np.array(train_x), np.array(train_y)
+        
+        mean_x = np.mean(train_x, axis=0)
+        std_x = np.std(train_x, axis=0)
+        train_x = (train_x - mean_x)/std_x
+
+        mean_y = np.mean(train_y, axis=0)
+        std_y = np.std(train_y, axis=0)
+        train_y = (train_y - mean_y)/std_y
+        #离散分类化
+
+        train_y=np.array([list(map(self.categorize,i)) for i in train_y.tolist()])
 
         train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=self.config.valid_data_rate,
                                                               random_state=self.config.random_seed,
                                                               shuffle=self.config.shuffle_train_data)   # 划分训练和验证集，并打乱
         return train_x, valid_x, train_y, valid_y
 
-    def get_test_data(self, return_label_data=False):
-        feature_data = self.norm_data[self.train_num:]
-        self.start_num_in_test = feature_data.shape[0] % self.config.time_step  # 这些天的数据不够一个time_step
-        time_step_size = feature_data.shape[0] // self.config.time_step
 
-        # 在测试数据中，每time_step行数据会作为一个样本，两个样本错开time_step行
-        # 比如：1-20行，21-40行。。。到数据末尾。
-        test_x = [feature_data[self.start_num_in_test+i*self.config.time_step : self.start_num_in_test+(i+1)*self.config.time_step]
-                   for i in range(time_step_size)]
-        if return_label_data:       # 实际应用中的测试集是没有label数据的
-            label_data = self.norm_data[self.train_num + self.start_num_in_test:, self.config.label_in_feature_index]
-            return np.array(test_x), label_data
-        return np.array(test_x)
+    
+    def get_test_data(self, return_label_data=False):
+        feature_data = self.x_data[self.train_num:]
+        label_data=self.y_data[self.train_num:]
+
+        rolling_win=[]
+        valid_x=[]
+        valid_y=[]
+        for i in range(len(feature_data)-self.config.predict_day):
+            rolling_win.append(feature_data[i])
+            if len(rolling_win)==self.config.time_step: 
+                valid_x.append(rolling_win)
+                rolling_win=rolling_win[1:]
+                valid_y.append((label_data[i+self.config.predict_day]-label_data[i])/label_data[i])
+            
+        mean_x = np.mean(valid_x, axis=0)
+        std_x = np.std(valid_x, axis=0)
+        valid_x = (valid_x - mean_x)/std_x
+
+        mean_y = np.mean(valid_y, axis=0)
+        std_y = np.std(valid_y, axis=0)
+        valid_y = (valid_y - mean_y)/std_y
+        valid_y=np.array([list(map(self.categorize,i)) for i in valid_y.tolist()])
+
+        return np.array(valid_x),np.array(valid_y)
 
 def load_logger(config):
     logger = logging.getLogger()
-    logger.setLevel(level=logging.DEBUG)
+    logger.setLevel(level=logging.DEBUG)#所有信息输出到控制台
 
     # StreamHandler
     stream_handler = logging.StreamHandler(sys.stdout)
@@ -192,47 +223,28 @@ def load_logger(config):
 
     return logger
 
-def draw(config: Config, origin_data: Data, logger, predict_norm_data: np.ndarray):
-    label_data = origin_data.data[origin_data.train_num + origin_data.start_num_in_test : ,
-                                            config.label_in_feature_index]
-    predict_data = predict_norm_data * origin_data.std[config.label_in_feature_index] + \
-                   origin_data.mean[config.label_in_feature_index]   # 通过保存的均值和方差还原数据
-    assert label_data.shape[0]==predict_data.shape[0], "The element number in origin and predicted data is different"
+def draw(config: Config, origin_data: Data, logger, predict_data: np.ndarray, label_data):
+    
+    predict_data=[i[0][0] for i in predict_data]
+    label_data=[i[0] for i in label_data]
 
-    label_name = [origin_data.data_column_name[i] for i in config.label_in_feature_index]
-    label_column_num = len(config.label_columns)
+    plt.figure(1)                     # 预测数据绘制
+    plt.plot(range(len(label_data)), label_data, label='label')
+    plt.plot(range(len(predict_data)), predict_data, label='predict')
+    #plt.title("Predict stock {} price with {}".format(label_name[i], config.used_frame))
+    plt.legend(loc='upper left')
 
-    # label 和 predict 是错开config.predict_day天的数据的
-    # 下面是两种norm后的loss的计算方式，结果是一样的，可以简单手推一下
-    # label_norm_data = origin_data.norm_data[origin_data.train_num + origin_data.start_num_in_test:,
-    #              config.label_in_feature_index]
-    # loss_norm = np.mean((label_norm_data[config.predict_day:] - predict_norm_data[:-config.predict_day]) ** 2, axis=0)
-    # logger.info("The mean squared error of stock {} is ".format(label_name) + str(loss_norm))
+    # logger.info("The predicted stock {} for the next {} day(s) is: ".format(label_name[i], config.predict_day) +
+    #         str(np.squeeze(predict_data[-config.predict_day:, i])))
+    if config.do_figure_save:
+        plt.savefig(config.figure_save_path+"{}predict_{}_with_{}.png".format(config.continue_flag, label_name[i], config.used_frame))
 
-    loss = np.mean((label_data[config.predict_day:] - predict_data[:-config.predict_day] ) ** 2, axis=0)
-    loss_norm = loss/(origin_data.std[config.label_in_feature_index] ** 2)
-    logger.info("The mean squared error of stock {} is ".format(label_name) + str(loss_norm))
-
-    label_X = range(origin_data.data_num - origin_data.train_num - origin_data.start_num_in_test)
-    predict_X = [ x + config.predict_day for x in label_X]
-
-    if not sys.platform.startswith('linux'):    # 无桌面的Linux下无法输出，如果是有桌面的Linux，如Ubuntu，可去掉这一行
-        for i in range(label_column_num):
-            plt.figure(i+1)                     # 预测数据绘制
-            plt.plot(label_X, label_data[:, i], label='label')
-            plt.plot(predict_X, predict_data[:, i], label='predict')
-            plt.title("Predict stock {} price with {}".format(label_name[i], config.used_frame))
-            logger.info("The predicted stock {} for the next {} day(s) is: ".format(label_name[i], config.predict_day) +
-                  str(np.squeeze(predict_data[-config.predict_day:, i])))
-            if config.do_figure_save:
-                plt.savefig(config.figure_save_path+"{}predict_{}_with_{}.png".format(config.continue_flag, label_name[i], config.used_frame))
-
-        plt.show()
+    plt.show()  
 
 def main(config):
     logger = load_logger(config)
     try:
-        np.random.seed(config.random_seed)  # 设置随机种子，保证可复现
+        np.random.seed(config.random_seed)  # 设置随机种子
         data_gainer = Data(config)
 
         if config.do_train:
@@ -241,10 +253,15 @@ def main(config):
 
         if config.do_predict:
             test_X, test_Y = data_gainer.get_test_data(return_label_data=True)
-            pred_result = predict(config, test_X)       # 这里输出的是未还原的归一化预测数据
-            draw(config, data_gainer, logger, pred_result)
+            pred_result = predict(config, test_X)
+
+            pred_result=[np.argwhere(i==max(i))  for i in pred_result]
+           
+
+            draw(config, data_gainer, logger, pred_result, test_Y)
     except Exception:
         logger.error("Run Error", exc_info=True)
+
 
 
 if __name__=="__main__":
--- model/model_pytorch.py
+++ model/model_pytorch.py
@@ -1,32 +1,30 @@
 # -*- coding: UTF-8 -*-
-"""
-@author: hichenway
-@contact: lyshello123@163.com
-@time: 2020/5/9 17:00
-@license: Apache
-pytorch 模型
-"""
-
 import torch
 from torch.nn import Module, LSTM, Linear
 from torch.utils.data import DataLoader, TensorDataset
+import torch.nn.functional as F
 import numpy as np
+torch.autograd.set_detect_anomaly(True)
 
 class Net(Module):
-    '''
-    pytorch预测模型，包括LSTM时序预测层和Linear回归输出层
-    可以根据自己的情况增加模型结构
-    '''
     def __init__(self, config):
         super(Net, self).__init__()
         self.lstm = LSTM(input_size=config.input_size, hidden_size=config.hidden_size,
-                         num_layers=config.lstm_layers, batch_first=True, dropout=config.dropout_rate)
-        self.linear = Linear(in_features=config.hidden_size, out_features=config.output_size)
+                         num_layers=config.lstm_layers, batch_first=False, dropout=config.dropout_rate)
+                         #pytorch rnn 参数顺序默认(seq_len, batch_size, input_size)，设置batch_first=True 变为batch seqlen inputsize
+        self.fc1 = Linear(in_features=config.hidden_size, out_features=64)
+        self.fc2 = Linear(in_features=64, out_features=3)
+        #self.relu=Linear()
 
     def forward(self, x, hidden=None):
+        x=x.permute(1,0,2)
         lstm_out, hidden = self.lstm(x, hidden)
-        linear_out = self.linear(lstm_out)
-        return linear_out, hidden
+        lstm_out=lstm_out[-1,:,:]
+        x=F.relu(self.fc1(lstm_out))
+        x=F.relu(self.fc2(x))
+        #x=F.softmax(x,dim=2)
+        
+        return x
 
 
 def train(config, logger, train_and_valid_data):
@@ -35,41 +33,40 @@ def train(config, logger, train_and_valid_data):
         vis = visdom.Visdom(env='model_pytorch')
 
     train_X, train_Y, valid_X, valid_Y = train_and_valid_data
-    train_X, train_Y = torch.from_numpy(train_X).float(), torch.from_numpy(train_Y).float()     # 先转为Tensor
-    train_loader = DataLoader(TensorDataset(train_X, train_Y), batch_size=config.batch_size)    # DataLoader可自动生成可训练的batch数据
 
-    valid_X, valid_Y = torch.from_numpy(valid_X).float(), torch.from_numpy(valid_Y).float()
+    train_X, train_Y = torch.from_numpy(train_X).float(), torch.from_numpy(train_Y).long().squeeze()     # 先转为Tensor
+    train_loader = DataLoader(TensorDataset(train_X, train_Y), batch_size=config.batch_size)    # DataLoader可自动生成可训练的batch数据
+    
+    valid_X, valid_Y = torch.from_numpy(valid_X).float(), torch.from_numpy(valid_Y).long().squeeze()
     valid_loader = DataLoader(TensorDataset(valid_X, valid_Y), batch_size=config.batch_size)
 
     device = torch.device("cuda:0" if config.use_cuda and torch.cuda.is_available() else "cpu") # CPU训练还是GPU
     model = Net(config).to(device)      # 如果是GPU训练， .to(device) 会把模型/数据复制到GPU显存中
-    if config.add_train:                # 如果是增量训练，会先加载原模型参数
-        model.load_state_dict(torch.load(config.model_save_path + config.model_name))
+
     optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
-    criterion = torch.nn.MSELoss()      # 这两句是定义优化器和loss
+    criterion = torch.nn.CrossEntropyLoss(reduction='mean')
 
     valid_loss_min = float("inf")
     bad_epoch = 0
     global_step = 0
     for epoch in range(config.epoch):
         logger.info("Epoch {}/{}".format(epoch, config.epoch))
-        model.train()                   # pytorch中，训练时要转换成训练模式
+        model.train()                   # pytorch中，训练时要转换成训练模式，启用batchnormalization和dropout；对应还有eval()模式
         train_loss_array = []
         hidden_train = None
+         
         for i, _data in enumerate(train_loader):
             _train_X, _train_Y = _data[0].to(device),_data[1].to(device)
-            optimizer.zero_grad()               # 训练前要将梯度信息置 0
-            pred_Y, hidden_train = model(_train_X, hidden_train)    # 这里走的就是前向计算forward函数
+            pred_Y = model(_train_X, hidden_train)   
+            
+            # 交叉熵的几个坑 1. target必须是long类型，2. target不能有多余的为1的维度
+            # 3. target不是one hot类型，而是列别编码，且为0到c-1
 
-            if not config.do_continue_train:
-                hidden_train = None             # 如果非连续训练，把hidden重置即可
-            else:
-                h_0, c_0 = hidden_train
-                h_0.detach_(), c_0.detach_()    # 去掉梯度信息
-                hidden_train = (h_0, c_0)
             loss = criterion(pred_Y, _train_Y)  # 计算loss
-            loss.backward()                     # 将loss反向传播
+            optimizer.zero_grad() #将梯度信息置为0
+            loss.backward()    # 将loss反向传播
             optimizer.step()                    # 用优化器更新参数
+
             train_loss_array.append(loss.item())
             global_step += 1
             if config.do_train_visualized and global_step % 100 == 0:   # 每一百步显示一次
@@ -82,9 +79,12 @@ def train(config, logger, train_and_valid_data):
         hidden_valid = None
         for _valid_X, _valid_Y in valid_loader:
             _valid_X, _valid_Y = _valid_X.to(device), _valid_Y.to(device)
-            pred_Y, hidden_valid = model(_valid_X, hidden_valid)
-            if not config.do_continue_train: hidden_valid = None
-            loss = criterion(pred_Y, _valid_Y)  # 验证过程只有前向计算，无反向传播过程
+            pred_Y = model(_valid_X, hidden_valid)
+
+            #if not config.do_continue_train: hidden_valid = None
+           
+            loss = criterion(pred_Y, _valid_Y)  # 计算loss
+             # 验证过程只有前向计算，无反向传播过程
             valid_loss_array.append(loss.item())
 
         train_loss_cur = np.mean(train_loss_array)
@@ -127,9 +127,8 @@ def predict(config, test_X):
     hidden_predict = None
     for _data in test_loader:
         data_X = _data[0].to(device)
-        pred_X, hidden_predict = model(data_X, hidden_predict)
+        pred_Y = model(data_X, hidden_predict)
         # if not config.do_continue_train: hidden_predict = None    # 实验发现无论是否是连续训练模式，把上一个time_step的hidden传入下一个效果都更好
-        cur_pred = torch.squeeze(pred_X, dim=0)
-        result = torch.cat((result, cur_pred), dim=0)
+        result = torch.cat((result, pred_Y), dim=0)
 
     return result.detach().cpu().numpy()    # 先去梯度信息，如果在gpu要转到cpu，最后要返回numpy数据
